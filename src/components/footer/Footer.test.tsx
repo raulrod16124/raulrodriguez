adcfa7232c467d64e29980b98ede7608ec1ced73
raul rodriguez <raul.rod16124@gmail.com>
2024-09-29 00:56:25 +0100

    install vitest and try first test

---
+++ src/components/footer/Footer.test.tsx
@@ -0,0 +1,7 @@
+import { describe, test, expect } from "vitest"
+
+describe("Footer tests", () => {
+    test("should render the Footer component", () => {
+        expect(true).toBeTruthy()
+    })
+})
