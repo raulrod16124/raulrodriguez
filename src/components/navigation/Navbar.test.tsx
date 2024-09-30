1006dfb86357f3ad8fdd8a473682c673d944a5fd
raul rodriguez <raul.rod16124@gmail.com>
2024-09-30 23:27:06 +0100

    create navbar unit tests

---
+++ src/components/navigation/Navbar.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from "@testing-library/react";
+import { describe, test, expect } from "vitest"
+import { Navbar } from "./Navbar";
+import "@testing-library/jest-dom";
+
+describe("Navbar tests", () => {
+    test("should render the Navbar component", () => {
+        render(<Navbar />)
+        const navbarTestid = screen.getByTestId("Navbar");
+        expect(navbarTestid).toBeInTheDocument();
+    })
+    test("should find the text 'home' twice in the document", () => {
+        render(<Navbar />)
+        const homeText = screen.getAllByText("home");
+        expect(homeText).toHaveLength(2);
+    })
+    test("should find the text 'about me' twice in the document", () => {
+        render(<Navbar />)
+        const aboutmeText = screen.getAllByText("about me");
+        expect(aboutmeText).toHaveLength(2);
+    })
+    test("should find the text 'projects' twice in the document", () => {
+        render(<Navbar />)
+        const projectsText = screen.getAllByText("projects");
+        expect(projectsText).toHaveLength(2);
+    })
+    test("should find the text 'contact' twice in the document", () => {
+        render(<Navbar />)
+        const contactText = screen.getAllByText("contact");
+        expect(contactText).toHaveLength(2);
+    })
+})
--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -13,29 +13,29 @@ export const Navbar = () => {
                         onClick={() => setScreenType("home")}
                         active={screenType === "home"}
                     >
-                        <NavButtonCurrentText className="text-current">Home</NavButtonCurrentText>
-                        <NavButtonNewText className="text-new">Home</NavButtonNewText>
+                        <NavButtonCurrentText className="text-current">home</NavButtonCurrentText>
+                        <NavButtonNewText className="text-new">home</NavButtonNewText>
                     </NavButton>
                     <NavButton
                         onClick={() => setScreenType("about")}
                         active={screenType === "about"}
                     >
-                        <NavButtonCurrentText className="text-current">About Me</NavButtonCurrentText>
-                        <NavButtonNewText className="text-new">About Me</NavButtonNewText>
+                        <NavButtonCurrentText className="text-current">about me</NavButtonCurrentText>
+                        <NavButtonNewText className="text-new">about me</NavButtonNewText>
                     </NavButton>
                     <NavButton
                         onClick={() => setScreenType("projects")}
                         active={screenType === "projects"}
                     >
-                        <NavButtonCurrentText className="text-current">Projects</NavButtonCurrentText>
-                        <NavButtonNewText className="text-new">Projects</NavButtonNewText>
+                        <NavButtonCurrentText className="text-current">projects</NavButtonCurrentText>
+                        <NavButtonNewText className="text-new">projects</NavButtonNewText>
                     </NavButton>
                     <NavButton
                         onClick={() => setScreenType("contact")}
                         active={screenType === "contact"}
                     >
-                        <NavButtonCurrentText className="text-current">Contact</NavButtonCurrentText>
-                        <NavButtonNewText className="text-new">Contact</NavButtonNewText>
+                        <NavButtonCurrentText className="text-current">contact</NavButtonCurrentText>
+                        <NavButtonNewText className="text-new">contact</NavButtonNewText>
                     </NavButton>
                 </NavButtons>
             </NavbarContainer>
