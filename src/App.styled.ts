8be921916995f310c7904e468d86b87e35f1beca
raul rodriguez <raul.rod16124@gmail.com>
2024-10-01 13:34:18 +0100

    implement responsive styles in the home section

--- src/App.styled.ts
+++ src/App.styled.ts
@@ -1,22 +1,20 @@
 import styled from 'styled-components';
 import theme from "./theme/theme.json";
 
-export const ScreenWrapper = styled.div`
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-`;
-
 export const GradientBackground = styled.div`
   background: radial-gradient(circle at center, 
     ${theme.colors.tertiary.light} 0%, 
     ${theme.colors.tertiary.main} 40%, 
     ${theme.colors.tertiary.dark} 100%);
-  min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   color: ${theme.colors.neutral.white};
   font-family: ${theme.font.primary.fontFamily};
+  min-height: 100vh;
+
+  @media (max-width: ${theme.responsive.laptops}) {
+    align-items: flex-start;
+    padding-top: 200px;
+  }
 `;
--- src/screens/home/Home.styled.ts
+++ src/screens/home/Home.styled.ts
@@ -49,16 +49,22 @@ export const HomeSection = styled.section`
   display: flex;
   justify-content: center;
   align-items: center;
-  max-width: 1270px;
 `;
 
 export const TitleContainer = styled.div`
-  margin: 0;
   margin-left: -150px;
   display: flex;
   flex-direction: column;
   height: fit-content;
   z-index: 1;
+
+  @media (max-width: ${theme.responsive.largeMobileDevices}) {
+    margin-left: 0;
+    margin-top: 120px;
+  }
+  @media (max-width: ${theme.responsive.smallMobileDevices}) {
+    margin-left: 0;
+  }
 `
 export const NormalText = styled.span`
   margin: 0;
@@ -69,6 +75,13 @@ export const NormalText = styled.span`
   line-height: 1;
   color: ${theme.colors.neutral.darkGrey};
   animation: ${fadeInUp} 1.2s ease-out forwards;
+
+  @media (max-width: ${theme.responsive.laptops}) {
+    font-size: 2rem;
+  }
+  @media (max-width: ${theme.responsive.largeMobileDevices}) {
+    font-size: 1.5rem;
+  }
 `;
 
 export const FirstBigText = styled.span`
@@ -84,6 +97,13 @@ export const FirstBigText = styled.span`
     -2px 2px 0 ${theme.colors.tertiary.main},  /* Top-left */
     2px -2px 0 ${theme.colors.tertiary.main};  /* Bottom-right */
   animation: ${fadeInUp} 1.2s ease-out forwards;
+
+  @media (max-width: ${theme.responsive.laptops}) {
+    font-size: 8rem;
+  }
+  @media (max-width: ${theme.responsive.largeMobileDevices}) {
+    font-size: 4rem;
+  }
 `;
 
 export const SecondBigText = styled.span`
@@ -104,6 +124,13 @@ export const SecondBigText = styled.span`
     ${fadeInUp} 1.2s ease-out forwards,
     ${typing} 1.2s steps(30, end) forwards,
     ${blink} 0.75s step-end infinite;
+
+  @media (max-width: ${theme.responsive.laptops}) {
+    font-size: 8rem;
+  }
+  @media (max-width: ${theme.responsive.largeMobileDevices}) {
+    font-size: 4rem;
+  }
 `;
 
 export const Image = styled.img`
@@ -117,10 +144,15 @@ export const Image = styled.img`
   border-radius: 50%;
   border: 2px solid ${theme.colors.secondary.main};
 
-  @media (max-width: 768px) {
-    width: 150px;
-    bottom: -10px;
-    right: -10px;
+  @media (max-width: ${theme.responsive.laptops}) {
+    top: -50px;
+    right: -180px;
+    width: 300px;
+  }
+  @media (max-width: ${theme.responsive.largeMobileDevices}) {
+    top: -100px;
+    right: 50px;
+    width: 200px;
   }
 `;
 
