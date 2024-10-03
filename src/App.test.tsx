1a07a05e8924ca0bdebfae8d3de49c118d942705
raul rodriguez <raul.rod16124@gmail.com>
2024-10-03 14:25:41 +0100

    fix the tests error with the useLocation()

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import { describe, test, expect } from "vitest"
+import { MemoryRouter } from "react-router-dom";
 import { App } from "./App";
 import "@testing-library/jest-dom";
 
 describe("App tests", () => {
     test("should render the App component", () => {
-        render(<App />)
+        render(
+            <MemoryRouter>
+                <App />
+            </MemoryRouter>
+        )
         const appTestid = screen.getByTestId("app");
         expect(appTestid).toBeInTheDocument();
     })
--- src/components/navigation/Navbar.test.tsx
+++ src/components/navigation/Navbar.test.tsx
@@ -1,31 +1,35 @@
 import { render, screen } from "@testing-library/react";
-import { describe, test, expect } from "vitest"
+import { describe, test, expect, beforeEach } from "vitest"
 import { Navbar } from "./Navbar";
+import { MemoryRouter } from 'react-router-dom';
 import "@testing-library/jest-dom";
+
+beforeEach(() => {
+    render(
+        <MemoryRouter>
+            <Navbar />
+        </MemoryRouter>
+    )
+})
 
 describe("Navbar tests", () => {
     test("should render the Navbar component", () => {
-        render(<Navbar />)
         const navbarTestid = screen.getByTestId("Navbar");
         expect(navbarTestid).toBeInTheDocument();
     })
     test("should find the text 'home' twice in the document", () => {
-        render(<Navbar />)
         const homeText = screen.getAllByText("home");
         expect(homeText).toHaveLength(2);
     })
     test("should find the text 'about me' twice in the document", () => {
-        render(<Navbar />)
         const about_meText = screen.getAllByText("about me");
         expect(about_meText).toHaveLength(2);
     })
     test("should find the text 'projects' twice in the document", () => {
-        render(<Navbar />)
         const projectsText = screen.getAllByText("projects");
         expect(projectsText).toHaveLength(2);
     })
     test("should find the text 'contact' twice in the document", () => {
-        render(<Navbar />)
         const contactText = screen.getAllByText("contact");
         expect(contactText).toHaveLength(2);
     })
