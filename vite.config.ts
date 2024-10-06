74104df305dd27a30a58b45b44487a46060c1658
raul rodriguez <raul.rod16124@gmail.com>
2024-10-06 21:21:29 +0100

    working to fix github pages bug 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: isProduction ? '/raulrodriguez/' : '/',
   assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg'],
   plugins: [react()],
 })
